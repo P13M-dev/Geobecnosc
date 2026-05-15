36eb0dca0c39dbe6dea914e4be5992b5328f0753
P13M-dev <p.m.labs084@gmail.com>
2026-05-15 19:57:06 +0200

    PHP

---
+++ Backend/PHP/Logowanie/nauczyciel.php
@@ -0,0 +1,22 @@
+<?php 
+
+session_start();
+
+if(!isset($_POST["email"])) die;
+if(!isset($_POST["haslo"])) die;
+
+$db = Database::get();
+
+$wynik = $db->query("SELECT id,hash_hasla,administrator FROM nauczyciele WHERE email = ?",[$_POST["email"]]);
+
+if(sizeof($wynik)==0) die; 
+if(!password_verify($_POST["haslo"],$wynik[0]["hash_hasla"])) die;
+
+$_SESSION["idNauczyciela"] = $wynik[0]["id"];
+$_SESSION["pozycja"] = $wynik[0]["administrator"] ? 2 : 1;
+
+
+
+
+?>
+
