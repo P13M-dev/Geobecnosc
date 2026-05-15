502b8c59b1104ffc150795480896d01ceeae38b8
G1ith <gabisp350@gmail.com>
2026-05-15 20:22:50 +0200

    Create obecnosci.php

---
+++ Backend/PHP/Wybieranie/obecnosci.php
@@ -0,0 +1,30 @@
+<?php 
+
+session_start();
+
+if(!isAdmin()) die;
+
+$dzien = $_POST["daySelect"];
+$klasa = $_POST["classSelect"];
+
+$db = Database::get();
+
+for($i=0; $i<10; $i++){
+    $query = "SELECT Uczen.Imie,Uczen.Nazwisko,
+    ObecnoscNaLekcji.spozniony,ObecnoscNaLekcji.obecny 
+
+    FROM ObecnoscNaLekcji
+
+    INNER JOIN ObecnoscWDniu ON ObecnoscWDniu.id = ObecnoscNaLekcji.ObecnoscWDniu_id
+    INNER JOIN Uczen ON Uczen.Id = ObecnoscWDniu.Uczen_Id
+    INNER JOIN Lekcja ON Lekcja.Godzina_idGodzina = ObecnoscNaLekcji.Lekcja_Godzina_idGodzina 
+
+    WHERE ObecnoscWDniu_Uczen_Klasa_id = $klasa
+    WHERE ObecnoscWDniu.Dzien_Dzien = $dzien
+    WHERE ObecnoscNaLekcji_Lekcja_Godzina_idGodzina = $i";
+}
+
+
+// echo json_encode($db->query($query));
+
+?>
